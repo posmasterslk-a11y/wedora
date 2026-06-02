36fd0bb470a479d82aaf7bc56de2eaf952790b32
Diluka Chamara <codehub.software@gmail.com>
2026-06-02 16:22:35 +0530

    Fix responsive issues on Signin and Signup by replacing h-screen with min-h-screen

--- app/pages/Auth/Signin.vue
+++ app/pages/Auth/Signin.vue
@@ -1,7 +1,7 @@
 <template>
   <FullScreenLayout>
     <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
-      <div class="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900">
+      <div class="relative flex flex-col justify-center w-full min-h-screen lg:flex-row dark:bg-gray-900">
         
         <div class="flex flex-col flex-1 w-full lg:w-1/2 justify-center">
           <div class="flex flex-col justify-center w-full max-w-md mx-auto px-4">
